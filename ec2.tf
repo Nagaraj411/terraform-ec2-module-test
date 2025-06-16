2eb01466c38a774387406e1c3fa2e62dcbf0c6b7
Nagaraj <nagarajuande255@gmail.com>
2025-06-16 23:46:22 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,6 @@
 module "ec2" {
-  source = "../terraform-module--aws-instance"
+  source        = "../terraform-module--aws-instance"
+  sg_ids        = var.sg_ids # This should match the security group ID in your tfvars files
+  instance_type = var.instance_type # This should match the instance type in your tfvars files
+  tags          = var.tags
 }
--- variables.tf
+++ variables.tf
@@ -1,20 +1,15 @@
-# variable "ami_id" {
-#   type        = string
-#   default     = "ami-09c813fb71547fc4f"
-#   description = "AMI ID of the EC2 Instance"
-# }
+variable "sg_ids" {
+  default = ["sg-009d0f9988cd2de9b"]
+}
 
-# variable "instance_type" {
-#   type = string
-#   default = "t3.micro"
-#   description = "Instance type for the EC2 Instance, can be overridden per environment"
-# }
+variable "tags" {
+  default = {
+    Name        = "roboshop-cart"
+    terraform   = "true"
+    environment = "dev"
+  }
+}
 
-# #mandatory variables to be passed from the command line
-# variable "sg_ids" {
-#   type        = list
-# }
-
-# variable "tags" {
-#   type        = map
-# }
+variable "instance_type" {
+    default = "t3.small"
+}
